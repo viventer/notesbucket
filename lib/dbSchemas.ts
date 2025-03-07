95c7a725e273e5c633d7a11b0bdabc2752be5d6f
viventer <viventermail@gmail.com>
2025-03-07 12:04:34 +0100

    adjusted schemas

--- lib/dbSchemas.ts
+++ lib/dbSchemas.ts
@@ -5,19 +5,19 @@ export const FolderSchema = z.object({
   id: z.string(),
   name: z
     .string()
-    .min(3, "Nazwa folderu musi zawierać min. 3 znaki")
-    .max(24, "Nazwa folderu nie może przekraczać 24 znaków"),
+    .min(1, "Nazwa folderu nie może być pusta")
+    .max(48, "Nazwa folderu nie może przekraczać 48 znaków"),
   parentFolderRef: z.custom<DocumentReference | null>(),
-  subFoldersRefs: z.array(z.custom<DocumentReference | null>()),
-  notesRefs: z.array(z.custom<DocumentReference | null>()),
+  subFoldersRefs: z.array(z.custom<DocumentReference>()).default([]),
+  notesRefs: z.array(z.custom<DocumentReference>()).default([]),
 });
 
 export const NoteSchema = z.object({
   id: z.string(),
   title: z
     .string()
-    .min(3, "Nazwa notatki musi zawierać min. 3 znaki")
-    .max(24, "Nazwa notatki nie może przekraczać 24 znaków"),
+    .min(1, "Nazwa notatki nie może być pusta")
+    .max(48, "Nazwa notatki nie może przekraczać 48 znaków"),
   mdContent: z
     .string()
     .max(10000, "Ilość znaków w notatce nie może przekraczać 10000 znaków"),
