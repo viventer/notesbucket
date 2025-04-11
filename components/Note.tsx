3d15879cbb38b74ae0192e7cbf753344afdd7dff
viventer <viventermail@gmail.com>
2025-04-11 14:49:29 +0200

    added no content info

--- components/Note.tsx
+++ components/Note.tsx
@@ -25,7 +25,13 @@ export default function Note({
       ) : (
         <article className="mb-[4rem] prose bg-background max-w-[1500px] xl:px-8 xl:py-6 xl:bg-card h-[75svh] sm:mb-[2rem] overflow-auto scrollbar-track-transparent scrollbar-thumb-primary scrollbar pr-2 mt-2">
           <h2>{title}</h2>
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+          {content ? (
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+          ) : (
+            <p className="text-lg underline underline-offset-4 decoration-accent">
+              Zawartość notatki nie została jeszcze uzupełniona.
+            </p>
+          )}
         </article>
       )}
     </div>
